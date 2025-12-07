7a103b14549db50a943ea7a366c11e5ae1adc20d
Nacho <imelero-@student.42madrid.com>
2025-12-07 21:44:39 +0100

    ex 0 ex 1

--- ex0/ft_command_quest.py
+++ ex0/ft_command_quest.py
@@ -1,3 +1,37 @@
 #!/usr/bin/env python3
 
+import sys
 
+
+def show_args() -> None:
+    """
+    Display the program name, each argument provided in the command line,
+    and the total number of arguments.
+
+    This function uses sys.argv, where:
+      - sys.argv[0] is always the program's name/path
+      - sys.argv[1:] are the arguments passed by the user
+    """
+
+    arg_nb = 1  # Counter to iterate through command‑line arguments
+    print("=== Command Quest ===")
+
+    # If only the program name is present, no arguments were passed
+    if len(sys.argv) == 1:
+        print("No arguments provided!")
+
+    # Always print the program's name
+    print(f"Program name: {sys.argv[0]}")
+
+    # Loop through all arguments passed (if any)
+    while arg_nb < len(sys.argv):
+        print(f"Argument {arg_nb}: {sys.argv[arg_nb]}")
+        arg_nb += 1
+
+    # Display total number of arguments including the program name
+    print(f"Total arguments: {len(sys.argv)}")
+
+
+if __name__ == "__main__":
+    # Entry point of the script
+    show_args()
---
+++ ex1/ft_score_analytics.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def show_scores(scores: list) -> None:
+
+    print("Scores processed: [", end="")
+    print(*scores, sep = ", ", end="")
+    print("]")
+
+
+def score_cruncher():
+
+    scores = []
+    argv_count = 1
+    player_count = 0
+    print("=== Player Score Analytics ===")
+    while argv_count < len(sys.argv):
+        try:
+            scores.append(int(sys.argv[argv_count]))
+            player_count += 1
+        except ValueError:
+            print(f"You must give numbers: '{sys.argv[argv_count]}' "
+                  "is not a number")
+        finally:
+            argv_count += 1
+
+    if not player_count:
+        print("No scores provided. Usage: python3 ft_score_analytics.py "
+              "<score1> <score2> ...")
+    else:
+        total_score = sum(scores)
+        average_score = (total_score / player_count)
+        high_score = max(scores)
+        low_score = min(scores)
+        score_range = high_score - low_score
+
+        show_scores(scores)
+        print("Total players:", player_count)
+        print("Total scores:", total_score)
+        print("Average score:", average_score)
+        print("High score:", high_score)
+        print("Low score:", low_score)
+        print("Score range:", score_range)
+
+
+if __name__ == "__main__":
+    score_cruncher()
