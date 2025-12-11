084a8dcba7db9bd40b0c8ecf5d890b771f877c81
Nacho <imelero-@student.42madrid.com>
2025-12-11 19:30:04 +0100

    ex4

--- ex4/ft_inventory_system.py
+++ ex4/ft_inventory_system.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 def show_inventory(inventory: dict):
 
     for name, data in inventory.items():
@@ -23,37 +22,97 @@ def show_inventory(inventory: dict):
         elif value['item'] == "armor":
             armor_count += value['amount']
 
-    print(f"Inventory value: {total_value} gold")
+    print(f"\nInventory value: {total_value} gold")
     print(f"Item count: {total_items} items")
     print(f"Categories: weapon({weapon_count}), consumable({consumable_count})"
           f", armor({armor_count})")
 
 
-def transaction(inventory1: dict, inventory2: dict, item: str, number: int):
+def transaction(inventories: dict, gives: str, recieves: str, item_given: str,
+                number: int) -> dict:
 
+    has_item = False
+    new_inventories = {
+            "Alice": {},
+            "Bob": dict(inventories[recieves]),
+            }
 
-    new_inventory = {}
-
-    for name, data in inventory1.items():
-        if name == item and data['amount'] < number:
-            print("Not enough amount")
+    try:
+        if inventories[gives][item_given]['amount'] < number:
+            print(f"Error. Can't give {number} {item_given} to {recieves}, {gives}" 
+                  f" has only {inventories[gives][item_given]['amount']}")
             return
-    for name, data in inventory1.items():
-        if name == item:
-            if data['amount'] > number:
+    except KeyError:
+        print(f"Error. {gives} has no {item_given}")
+        return
+
+    for name, data in inventories[recieves].items():
+        if name == item_given:
+                new_data = dict(data)
+                new_data['amount'] += number
+                new_inventories[recieves][name] = new_data
+                has_item = True
+        else:
+            new_inventories[recieves][name] = dict(data)
+    if has_item == False:
+        new_inventories[recieves][item_given] = inventories[gives][item_given]
+        new_inventories[recieves][item_given]['amount'] = number
+
+    for name, data in inventories[gives].items():
+        if name == item_given:
+            if inventories[gives][item_given]['amount'] > number:
                 new_data = dict(data)
                 new_data['amount'] -= number
-                new_inventory[name] = new_data
+                new_inventories[gives][name] = new_data
         else:
-            new_inventory[name] = data
+            new_inventories[gives][name] = dict(data)
+ 
+    return new_inventories
 
-    return new_inventory
-                
+
+def inventory_analytics(inventories: dict) -> None:
+
+    max_items = 0
+    max_value = 0
+    saved_name = ""
+    rarest_items = {}
+
+    for player in inventories:
+        tmp_max_value = 0
+        for name, data in inventories[player].items():
+            tmp_max_value += data['cost'] * data['amount']
+            if data['rarity'] == "rare":
+                rarest_items[name] = dict(data)
+        if tmp_max_value > max_value:
+            max_value = tmp_max_value
+            saved_name = player
+    print(f"Most valuable player: {saved_name} ({max_value})")
+
+    for player in inventories:
+        tmp_max_items = 0
+        for name, data in inventories[player].items():
+            tmp_max_items += data['amount']
+        if tmp_max_items > max_items:
+            max_items = tmp_max_items
+            saved_name = player
+
+    print(f"Most items: {saved_name} ({max_items} items)")
+
+    print(f"Rarest items:", end=" ")
+    first = True
+    for name in rarest_items.keys():
+        if not first:
+            print(", ", end="")
+        print(name)
+        print("", end="")
+
 
 def main():
 
     print("=== Player Inventory System ===")
     print("")
+
+    new_inventories = None
     alice_inventory = {
             "sword": {
                 "item": "weapon",
@@ -64,7 +123,7 @@ def main():
             "potion": {
                 "item": "consumable",
                 "rarity": "common",
-                "amount": 5,
+                "amount": 2,
                 "cost": 50,
                 },
             "shield": {
@@ -82,6 +141,12 @@ def main():
                 "amount": 1,
                 "cost": 500,
                 },
+            "potion": {
+                "item": "consumable",
+                "rarity": "common",
+                "amount": 2,
+                "cost": 50,
+                },
             "shield": {
                 "item": "armor",
                 "rarity": "uncommon",
@@ -89,13 +154,31 @@ def main():
                 "cost": 200,
                 }
             }
+    inventories = {
+            "Alice": alice_inventory,
+            "Bob": bob_inventory}
 
     print("=== Alice's Inventory ===")
     show_inventory(alice_inventory)
     print("")
+
     print("=== Transaction: Alice gives Bob 2 potions ===")
-    alice_inventory = transaction(alice_inventory, bob_inventory, "potion", 5)
-    print(alice_inventory)
+    new_inventories = transaction(inventories, "Alice", "Bob", "potion", 2)
+    if new_inventories:
+        inventories.update(new_inventories)
+        print("Transaction successful!")
+        print(f"\n=== Updated Inventories ===")
+        try:
+            print(f"Alice potions: {inventories['Alice']['potion']['amount']}")
+        except KeyError:
+            print("Alice potions: 0")
+        print(f"Bob potions: {inventories['Bob']['potion']['amount']}")
+    else:
+        print("Transaction failed!")
+    print("")
+
+    print("=== Inventory Analytics ===")
+    inventory_analytics(inventories)
 
 if __name__ == "__main__":
     main()
